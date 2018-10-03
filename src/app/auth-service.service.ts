63cf1633432927cf3867cdcbc1bb36be38980d2b
Weiyu <wshen4@fordham.edu>
2018-10-03 19:07:33 -0400

    fixed axios js import

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-const axios = require("axios");
+import axios from "axios";
 
 @Injectable({
   providedIn: "root"
